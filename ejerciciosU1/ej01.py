24e58cf342b428b4a78230e7557d2cc5ba93face
eperyaq205 <eperyaq205@g.educaand.es>
2023-10-09 18:21:16 +0200

    Ejercicios sin terminar

--- ejerciciosU1/ej01.py
+++ ejerciciosU1/ej01.py
@@ -1,1 +1,101 @@
-print("Hola")
+#print("123")
+#1.-
+#print ("Dime tu nombre")
+#nombre = input ()
+#print ("hola, " + nombre)
+
+
+#2.-
+#print("Cuantas horas has trabajado?")
+#horas = int (input ())
+#coste = 10
+#resultado = horas * coste
+#print("Importe total: " , resultado) 
+
+#3.-
+#ancho = 17
+#alto = 12.0
+#print ("1. ancho/2 = " , ancho / 2  )
+#print ("2. ancho//2 = " , ((ancho / 2) /2) )
+#print ("3. alto/3 = " , alto / 3  )
+#print ("4. 1+2*5 = " , 1+2*5 )
+
+#4.-
+#tempcelsius = 0
+#resultado = 0
+#tempcelsius= int(input("dame la temp a convertir: "))
+#print(  (tempcelsius * 9/5) + 32)
+
+#precio = 0
+#resultado = 0
+
+#5.-
+#precio = int(input("Dime el precio del articulo: "))
+#print (precio * 0.21)
+
+#6.-
+#precio = int(input("Dime el precio del articulo: "))
+#print ("El precio del importe sin iva es: " , precio - precio* 0.10)
+#print ("Has pagado de iva : " , precio * 0.10)
+
+
+
+#7.-
+#a=0 
+#b=0
+#c=0
+
+#print("Dime tres numeros monstro: ")
+#a=int(input("dime el primero: "))
+#b=int(input("dime el segundo: "))
+#c=int(input("dime el tercero: "))
+#print(a+b+c)
+
+
+
+#numeros=[1,2,3]
+#a,b,c= numeros
+#print(b)
+
+
+
+
+#2.10
+#resultado= pow((3+2/2*5), 2)
+#print(resultado)
+
+#2.11 ¿?
+#a=0
+#print("Dame un numero: ")
+#a=int(input())
+
+#for()
+#while a<=1 or a>1:
+#    resultado=((a*(a+1))/2)
+#    break
+
+#print("tu resultado es: " , resultado )
+#no se como funciona while en python 
+
+#2.12
+#peso = float(input("Dime tu peso en kg: "))
+#altura = float(input("Dime tu altura en metros: "))
+#imc = peso % (altura*altura)
+#print("Tu indice de masa corporal es: " , imc)
+
+#2.13 ¿?
+#n = int(input("Dime un numero: "))
+#m = int(input("Dime otro: "))
+#print("La división de" , n , "entre " , m ,  "da un cociente" )
+
+#2.14
+#payaso=int(input("Cuantos payasos quieres que haya en el pedido?: "))
+#muñecas=int(input("Y cuantas muñecas?: "))
+#peso=(payaso*112)+(muñecas*75)
+#print("El pedido va a pesar: " , peso , "gramos.")
+
+#2.15 ¿?
+#dinero=float(input("Cuanto dinero hay en la cuenta?: "))
+#interes= 0.4
+#total= dinero*interes
+#print("En el primer año tienes:")
