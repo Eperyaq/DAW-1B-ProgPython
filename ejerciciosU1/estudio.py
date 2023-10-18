46cd6319a95fd242a8508aeb665fffd4fc33fb47
eperyaq205 <eperyaq205@g.educaand.es>
2023-10-18 12:12:14 +0200

    estudio

---
+++ ejerciciosU1/estudio.py
@@ -0,0 +1,36 @@
+#2.4
+#celsius=int(input("Dime la temperatura en celsius: "))
+#farenheit=0
+#cambio= (celsius*9/5)+32
+#print("Los ", celsius,"ºc son ", cambio, "º farenheit.")
+
+#2.5
+#sin=float(input("Dime el precio del articulo SIN iva: "))
+#iva=float(input("Cuanto iva se aplica?: "))
+#precio= sin*iva/100
+#print("El precio de iva para ese objeto es ", precio)
+#print("El precio del objeto con iva es ", sin+precio)
+
+#1.32
+#num1=int(input("Dame un numero: "))
+#num2=int(input("Dame otro numero: "))
+
+#if (num1<num2):
+#    numini=num1
+#    numfin=num2
+#else:
+#    numini=num2
+#    numfin=num1
+#while(numini<=numfin):
+#    print(numini, end="")
+#    if(numini != numfin):
+#        print("-", end="")
+#    numini = numini + 1
+
+#p= len(input("palabra: "))
+#print("P tiene ", p, "caracteres")
+
+#d=float(input("Dime numeros decimales para sumar."))
+#e=float(input("Dime numeros decimales para sumar."))
+#suma=e+d
+#print("{:.2f}".format(suma))
